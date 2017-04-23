4d05d86508024481dcdfd84458913713b837f019
Andrea <apmansur1993@gmail.com>
2017-04-23 11:55:32 -0500

    addeding messages

--- app/scripts/controllers/ChatCtrl.js
+++ app/scripts/controllers/ChatCtrl.js
@@ -1,9 +1,16 @@
  (function () {
      function ChatCtrl(Room) {
-         this.rooms= Room.all;
+         this.rooms = Room.all;
+
+
+         $scope.setRoom = function (rooms, $index) {
+             $scope.currentRoom = rooms[$index];
+             $scope.messages = Message.getByRoomId($scope.currentRoom.$id);
+
+         };
      }
 
      angular
          .module('blocChat')
-         .controller('ChatCtrl', [ 'Room', ChatCtrl]);
+         .controller('ChatCtrl', ['Room','Messages','$scope' ChatCtrl]);
  })();
