b693e52fa2bb53d5e9ff5640840a58bcc0868582
Andrea <apmansur1993@gmail.com>
2017-04-26 22:16:33 -0500

    adding user name, still with bugs

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,18 +1,40 @@
- (function() {
+ (function () {
      function config($stateProvider, $locationProvider) {
          $locationProvider
-         .html5Mode({
-             enabled: true,
-             requireBase: false
-         });
+             .html5Mode({
+                 enabled: true,
+                 requireBase: false
+             });
          $stateProvider
-         .state('chat', {
-             url: '/',
-             controller: 'ChatCtrl as chat',
-             templateUrl: '/templates/chat.html'
-         });
+             .state('chat', {
+                 url: '/',
+                 controller: 'ChatCtrl as chat',
+                 templateUrl: '/templates/chat.html'
+             });
      }
+
+     function BlocChatCookies($cookies, $uibModal) {
+         var currentUser = $cookies.get('blocChatCurrentUser');
+
+         if (!currentUser || currentUser === '') {
+             var modalInstance = $uibModal.open({
+                 templateUrl: '/templates/username.html',
+                 controller: 'CookieInstanceCtrl',
+                 controllerAs: 'modalUser'
+             });
+
+             modalInstance.result.then(function (username) {
+                 this.username = username;
+                 $cookies.put('blocChatCurrentUser', this.username);
+                 var currentUser = $cookies.get('blocChatCurrentUser');
+                 console.log(currentUser);
+             });
+         }
+     }
+
+
      angular
-         .module('blocChat',['firebase','ui.router', 'ui.bootstrap'])
-         .config(config);
+         .module('blocChat', ['ngCookies', 'ui.bootstrap', 'ui.router', 'firebase'])
+         .config(config)
+         .run(['$cookies', '$uibModal', BlocChatCookies]);
  })();
---
+++ app/scripts/controllers/CookiesInstanceCtrl.js
@@ -0,0 +1,16 @@
+(function() {
+    function CookiesInstanceCtrl($uibModalInstance,$scope) {
+        
+       $scope.accept = function(isValid) {
+            
+            if (isValid) {
+                $uibModalInstance.close($scope.username);
+            }
+            
+        };
+    }
+    
+    angular
+        .module('blocChat')
+        .controller('CookiesInstanceCtrl', ['$uibModalInstance','$scope', CookiesInstanceCtrl]);
+})();
